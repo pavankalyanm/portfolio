4e94314ca0381a1e47a8f47e58f4acb686d484f6
pavankalyanm <m.pa1chinna@gmail.com>
2022-01-16 13:05:57 +0530

    name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => {
       <div class="row" >
         <div class="column"style={{marginTop:"20%"}}>
          
-          <h1 className='text-5xl font-bold md:text-7xl'>Hi There ! <br></br>I’m Pavan</h1>
+          <h1 className='text-5xl font-bold md:text-7xl'>Hi There ! <br></br>I’m Pavan Kalyan</h1>
           <br></br><h3>DEVELOPER | AI & ML ENTHUSIAST</h3>
           <br></br>
           <br></br>
